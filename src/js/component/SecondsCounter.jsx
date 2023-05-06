d3a8d5e907efe27386f9ec37275da08d5307eef4
julian19028 <121776944+julian19028@users.noreply.github.com>
2023-05-06 03:36:54 +0000

    almost done

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+//create your first component
+const SecondsCounter = (props) => {
+	return (
+		<div className="container text-center">
+			<div className="row border border-light p-3 mb-2 bg-black text-white">
+			 <p></p>
+             <p></p>
+			 <p></p>
+			 <p></p>
+			 <p></p>
+			 <p></p>
+			 <p></p>
+            </div>
+		</div>
+	);
+};
+// {props.sixthDigit}
+// {props.fifthDigit}
+// {props.fourthDigit
+// {props.thirdDigit}
+// {props.secondDigit}
+// {props.firstDigit}
+export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,51 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import SecondsCounter from "./component/SecondsCounter.jsx";
+
+let firstDigit = 0
+let secondDigit = 0
+let thirdDigit = 0
+let fourthDigit = 0
+let fifthDigit = 0
+let sixthDigit = 0
+
+setInterval(function()  {
+    firstDigit++;
+    if (firstDigit === 10) {
+        firstDigit = 0;
+        secondDigit++;
+        if (secondDigit ===10) {
+            secondDigit = 0;
+            thirdDigit++;
+            if (thirdDigit ===10) {
+                thirdDigit = 0;
+                fourthDigit++;
+                if (fourthDigit ===10) {
+                    fourthDigit = 0;
+                    fifthDigit++;
+                    if (fifthDigit ===10) {
+                        fifthDigit = 0 
+                        sixthDigit++;
+                        if (sixthDigit ===10) {
+                            sixthDigit = 0;
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+    console.log(firstDigit)
+    console.log(secondDigit)
+    console.log(thirdDigit)
+    console.log(fourthDigit)
+    console.log(fifthDigit)
+    console.log(sixthDigit)
+    ReactDOM.render(<SecondsCounter firstDigit={firstDigit} secondDigit= {secondDigit} thirdDigit = {thirdDigit} fourthDigit = {fourthDigit} fifthDigit = {fifthDigit} sixthDigit = {sixthDigit} />, document.querySelector("#app"));
+},
+1000
+)
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
